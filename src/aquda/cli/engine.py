699b5870bc860083e5d07ee134ede6930a02226d
Tao Ruangyam <tao.ruangyam@zalando.de>
2024-12-20 08:09:09 +0100

    report error run mode

--- src/aquda/cli/engine.py
+++ src/aquda/cli/engine.py
@@ -1,7 +1,29 @@
 import click
+import sys
+from typing import Any
+
+from . import colour
 
 @click.command()
-@click.option('--lang', default='en', help='Language to process')
-def run_cli(lang):
+@click.option('-gen', is_flag=True, default=False, help='Data generation mode')
+@click.option('-aug', is_flag=True, default=False, help='Data augmentation mode')
+@click.option('-validate', is_flag=True, default=False, help='Data validation mode')
+@click.option('-l', '--lang', default=['en'], show_default=True, 
+              multiple=True,
+              help='Language to process')
+def run_cli(gen: bool, aug: bool, validate: bool, lang: list[str]) -> int:
+    print(f'Gen = {gen}')
+    print(f'Aug = {aug}')
+    print(f'Validate = {validate}')
     print(f'Lang = {lang}')
-    return None
+    run_mode = get_run_mode(gen, aug, validate)
+    if run_mode < 0:
+        return -1
+    return 0
+
+def get_run_mode(gen: bool, aug: bool, validate: bool) -> int:
+    # One mode must be true
+    if sum([gen, aug, validate]) != 1:
+        sys.stderr.write(f'{colour.RED}ERROR: Run mode must be one of [-gen, -aug, -validate]{colour.DEFAULT}')
+        return -1
+    return 0
